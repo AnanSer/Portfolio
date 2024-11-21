e3ccd5bff2532c8f5168c51f803e9230e2cd4074
AnanSer <ananserbesa@gmail.com>
2024-11-21 23:38:17 +0300

    some style

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -1,11 +1,11 @@
 import images from "../../images/me.jpg";
 
-import "./HomePage.module.css";
+import { classes } from "./HomePage.module.css";
 
 const HomePage = () => {
   return (
-    <section className="homepage">
-      <h1 className="heading">web developer</h1>
+    <section className={classes.homepage}>
+      <h1 className={classes.heading}>web developer</h1>
 
       <img className="image" src={images} alt="" />
       <h3 className="person-name">Anan Serbesa</h3>
