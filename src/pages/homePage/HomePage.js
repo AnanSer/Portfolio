77ce1fd684474de52b20cd399f1022dc1edd3234
AnanSer <ananserbesa@gmail.com>
2024-11-22 00:31:49 +0300

    styled image in homepage

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -1,14 +1,14 @@
 import images from "../../images/me.jpg";
 
-import { classes } from "./HomePage.module.css";
+import classes from "./HomePage.module.css";
 
 const HomePage = () => {
   return (
     <section className={classes.homepage}>
       <h1 className={classes.heading}>web developer</h1>
 
-      <img className="image" src={images} alt="" />
-      <h3 className="person-name">Anan Serbesa</h3>
+      <img className={classes.image} src={images} alt="" />
+      <h3 className={classes["person-name"]}>Anan Serbesa</h3>
       <a href="#" className="btn">
         projects
       </a>
