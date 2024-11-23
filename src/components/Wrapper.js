6298d4d713ac83edcb98f5f4a6f52db73808c8ae
AnanSer <ananserbesa@gmail.com>
2024-11-24 00:18:36 +0300

    added About and style componen

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -3,38 +3,14 @@ import HomePage from "../pages/HomePage/HomePage";
 
 import classes from "./Wrapper.module.css";
 import NavBar from "./NavBar/NavBar";
+import About from "../pages/About/About";
+
 const Wrapper = () => {
   return (
     <div className={classes.all}>
       <NavBar />
       <HomePage />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <p>heelel</p>
-      <p>heelel</p>
-      <p>heelel</p>
-      <p>heelel</p>
-      <p>heelel</p>
+      <About />
     </div>
   );
 };
